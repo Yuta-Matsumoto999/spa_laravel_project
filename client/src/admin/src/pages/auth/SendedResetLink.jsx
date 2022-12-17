46ae0d1fb272d3d94b1f08ea56962ffd791bc7cb
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-12-18 04:09:43 +0900

    不要な関数等を削除

--- client/src/admin/src/pages/auth/SendedResetLink.jsx
+++ client/src/admin/src/pages/auth/SendedResetLink.jsx
@@ -1,74 +1,8 @@
 import React, { useEffect } from 'react'
-import { Box, Button, TextField, ThemeProvider, Typography } from '@mui/material';
-import { LoadingButton } from "@mui/lab";
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from 'react';
-import authApi from '../../api/authApi';
-import { SocialIcon } from 'react-social-icons';
-import { FaFacebookSquare, FaTwitterSquare, FaLine, FaGoogle } from "react-icons/fa";
-import { IconContext } from 'react-icons';
-import { color } from '@mui/system';
-import { purple } from '@mui/material/colors';
+import { Box, Button, Typography } from '@mui/material';
+import { Link } from "react-router-dom";
 
 const SendedResetLink = () => {
-    const navigate = useNavigate();
-
-    // validation error
-    const [emailValidateErr, setEmailValidateErr] = useState("");
-
-    const [loading, setLoading] = useState(false)
-
-    const handlePasswordReset = async (e) => {
-        e.preventDefault();
-
-        // initial validate error
-        setEmailValidateErr("");
-
-        // get form value
-        const formData = new FormData(e.target);
-        const email = formData.get("email").trim();
-
-        // validation
-
-        let validationErr = false
-
-        if(email === "") {
-            validationErr = true;
-            setEmailValidateErr("メールアドレスを入力してください。");
-        }
-
-        if(validationErr) return;
-
-        setLoading(true);
-
-        const accessPasswordResetApi = async () => {
-            try {
-                const user = await authApi.login({email});
-
-                if(user) {
-                    navigate("/")
-                }
-            } catch (err) {
-                setLoading(false);
-
-                const errors = Array(err.data.errors);
-                
-                errors.forEach((error) => {
-                    if(error.email) {
-                        setEmailValidateErr(error.email[0]);
-                    }
-                })
-            }
-        }
-
-        // initialize csrf token and access passwordReset api
-        await authApi.initialCsrfToken().then((res) => {
-            accessPasswordResetApi();
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
-
     return (
         <>
         <Typography sx={{ marginBottom: "20px", fontWeight: "800", fontSize: "1.4rem" }}>Sended Reset Link</Typography>
